0b777003924bdc9d177f74110a3ec770e7e1c3bc
Chris Moore <35407569+csm123199@users.noreply.github.com>
2020-02-20 14:46:32 -0600

    Convert to Material Components Web Components

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 
 import './index.scss';
 import '@fortawesome/fontawesome-free/css/all.css';
+/*
 import { MDCTextField } from '@material/textfield';
 import { MDCRipple } from '@material/ripple';
 import { MDCTabBar } from '@material/tab-bar';
@@ -13,14 +14,14 @@ import { MDCTabBar } from '@material/tab-bar';
 // Essentially our 'page has loaded!' event
 //document.addEventListener('MDCAutoInit:End', () => {
 
-	/** Initialize MDC Web components. */
+	// Initialize MDC Web components.
 	const greetButtonEl = document.getElementById('container-greet-button');
 	MDCRipple.attachTo(greetButtonEl);
 
 	const fnameTextfield = new MDCTextField(document.getElementById('container-fname-text-field'));
 	const lnameTextfield = new MDCTextField(document.getElementById('container-lname-text-field'));
 
-	/** Custom javascript code. */
+	// Custom javascript code.
 	const greetMessageEl = document.getElementById('container-greet-message');
 
 	greetButtonEl.addEventListener('click', () => {
@@ -44,5 +45,50 @@ import { MDCTabBar } from '@material/tab-bar';
 	]
 	const tabBar = new MDCTabBar(document.getElementById('container-osTabs'))
 //})
+*/
 
+import '@material/mwc-tab-bar'
+import '@material/mwc-tab'
+import '@material/mwc-button'
+import '@material/mwc-textfield'
+//import '@material/mwc-typography'
+import { TextField as MDCTextField } from '@material/mwc-textfield';
+import { TabBar as MDCTabBar } from '@material/mwc-tab-bar';
 
+const nameFirst = document.getElementById('nameFirst') as MDCTextField;
+const nameLast = document.getElementById('nameLast') as MDCTextField;
+const btnGreet = document.getElementById('btnGreet');
+const txtGreet = document.getElementById('txtGreet');
+
+btnGreet.addEventListener('click', () => {
+	let name = [nameFirst.value, nameLast.value].join(' ');
+	txtGreet.textContent = `Hello, ${ name.trim().length != 0 ? name : 'Anonymous'}!`
+})
+
+const tabsChooseOS = document.getElementById('tabsChooseOS') as MDCTabBar;
+tabsChooseOS.addEventListener('MDCTabBar:activated', (ce: CustomEvent) => {
+	const index = ce.detail.index;
+	
+	// Note: 0 = windows, 1 = macos/linux
+	let enableWindows = false;
+	let enableUnix = false;
+	if(index === 0) {
+		enableWindows = true;
+	} else if(index === 1) {
+		enableUnix = true;
+	} else {
+		throw new Error('Unknown OS tab index: ' + index);
+	}
+	
+	let clsWindows = Array.from(document.getElementsByClassName('windows'));
+	let clsUnix = Array.from(document.getElementsByClassName('unix'));
+
+	let logic = enabled =>
+		e => {
+			if(enabled) e.removeAttribute('hidden');
+			else e.setAttribute('hidden', '');
+		}
+
+	clsWindows.forEach(logic(enableWindows));
+	clsUnix.forEach(logic(enableUnix));
+});
