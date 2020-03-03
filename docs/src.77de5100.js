4b7695d01f8cdf339d189bf81145ce70799593ec
Chris Moore <35407569+csm123199@users.noreply.github.com>
2020-03-02 21:35:35 -0600

    set parcel to use wrd204-mctutorial for the public root directory

--- docs/src.77de5100.js
+++ docs/src.77de5100.js
@@ -35856,7 +35856,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57847" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58278" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
